929d999b4898a76bf955eff5f0f84bad21bcc48a
Melvin Liji Abraham <melvin.lijiabraham@thoughtworks.com>
2025-07-03 19:40:48 +0530

    Fix build errors due to compiling storybook files
    Exclude storybook and vitest configs from being compiled by tsc

--- dist/stories/Button.stories.d.ts
+++
@@ -1,9 +0,0 @@
-import type { StoryObj } from '@storybook/web-components-vite';
-import type { ButtonProps } from './Button';
-declare const meta: Meta<ButtonProps>;
-export default meta;
-type Story = StoryObj<ButtonProps>;
-export declare const Primary: Story;
-export declare const Secondary: Story;
-export declare const Large: Story;
-export declare const Small: Story;
--- dist/stories/Page.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import type { StoryObj } from '@storybook/web-components-vite';
-import type { PageProps } from './Page';
-declare const meta: Meta<PageProps>;
-export default meta;
-type Story = StoryObj<PageProps>;
-export declare const LoggedIn: Story;
-export declare const LoggedOut: Story;
