9a34467de67dc3df835088d2c22e71d83e0077b9
Melvin Liji Abraham <melvin.lijiabraham@thoughtworks.com>
2025-07-03 18:14:27 +0530

    Add release GH actions workflow

---
+++ dist/stories/Header.stories.d.ts
@@ -0,0 +1,7 @@
+import type { StoryObj } from '@storybook/web-components-vite';
+import type { HeaderProps } from './Header';
+declare const meta: Meta<HeaderProps>;
+export default meta;
+type Story = StoryObj<HeaderProps>;
+export declare const LoggedIn: Story;
+export declare const LoggedOut: Story;
---
+++ dist/stories/Page.stories.d.ts
@@ -0,0 +1,7 @@
+import type { StoryObj } from '@storybook/web-components-vite';
+import type { PageProps } from './Page';
+declare const meta: Meta<PageProps>;
+export default meta;
+type Story = StoryObj<PageProps>;
+export declare const LoggedIn: Story;
+export declare const LoggedOut: Story;
